57cfec2888fe482c4eaedac5ec45bc9325784301
Peter Smit <peter@cis.hut.fi>
2011-06-10 14:40:51 +0300

    Boilerplate

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='smit.samplepyramid',
+      version='0.0.1dev',
+      description='Sample pyramid app',
+      author='Peter Smit',
+      author_email='peter@smitmail.eu',
+      packages=['smit'],
+      package_dir={'': 'src'},
+      install_requires=[],
+      entry_points = dict()
+      )
